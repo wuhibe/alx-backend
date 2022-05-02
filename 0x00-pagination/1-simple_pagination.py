6c494909e9d7f715e009019a37a333c67656df58
wuhibe <wuhibetamire710@gmail.com>
2022-05-02 20:20:03 +0300

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,32 @@
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        ''' method to return page '''
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        lst = self.dataset()
+        page, page_size = index_range(page, page_size)
+        return lst[page: page_size]
