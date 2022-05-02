b47353d693f49aa600507507e8e3bcef281e5a38
wuhibe <wuhibetamire710@gmail.com>
2022-05-02 20:02:17 +0300

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 ''' module for task 0 '''
 
+
 def index_range(page: int, page_size: int) -> tuple:
     ''' start to end of last index page '''
+    if page < 1 or page_size < 1:
+        return (0, 0)
     last: int = page_size * page
     return (last - page_size, last)
