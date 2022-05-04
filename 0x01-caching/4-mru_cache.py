70f409bd06389414c6c2c6caa23751682c6c2ba0
wuhibe <wuhibetamire710@gmail.com>
2022-05-05 01:05:35 +0300

    use list

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -8,34 +8,23 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ Initialize class """
         super().__init__()
-        self.store = {}
-        self.recent = 0
+        self.__keys = []
 
     def put(self, key, item):
         """ Assign to the dictionary """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop()
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
         if key and item:
+            if key not in self.__keys:
+                self.__keys.append(key)
             self.cache_data[key] = item
-            self.store[key] = self.recent
-            self.recent += 1
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            recent = 0
-            # store key in temp to prevent removal
-            temp = key
-            # find the key with the highest recent value
-            for ky in self.store:
-                rec = self.store.get(ky)
-                if rec > recent and ky != temp:
-                    recent = rec
-                    key = ky
-            print("DISCARD: {}".format(key))
-            # remove the key from cache and store
-            self.cache_data.pop(key)
-            self.store.pop(key)
 
     def get(self, key):
         """ Return the value linked """
-        if key is None or self.cache_data.get(key) is None:
+        if not key or key not in self.cache_data:
             return None
-        self.store[key] = self.recent
-        self.recent += 1
-        return self.cache_data.get(key)
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
