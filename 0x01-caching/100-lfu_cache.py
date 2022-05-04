56f754497452ed370ea14d6f3fa067a114a9916c
wuhibe <wuhibetamire710@gmail.com>
2022-05-05 01:40:34 +0300

    final

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ LFU caching """
+from cmath import inf
 from base_caching import BaseCaching
 
 
@@ -21,23 +22,25 @@ class LFUCache(BaseCaching):
             self.lst.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             keys = []
-            least = self.recent
+            temp = key
+            least = float(inf)
             val = self.recent
             for x in self.store:
-                if self.lst.count(x) < least:
+                if self.lst.count(x) < least and x != temp:
                     keys.clear()
                     least = self.lst.count(x)
                     keys.append(x)
-                elif self.lst.count(x) == least:
+                elif self.lst.count(x) == least  and x != temp:
                     keys.append(x)
             for x in keys:
                 vl = self.store[x]
-                if vl < val:
+                if vl < val  and x != temp:
                     val = vl
                     key = x
             print("DISCARD: {}".format(key))
             self.cache_data.pop(key)
             self.store.pop(key)
+            self.lst = list(filter(lambda a: a != key, self.lst))
 
     def get(self, key):
         """ Return the value linked """
