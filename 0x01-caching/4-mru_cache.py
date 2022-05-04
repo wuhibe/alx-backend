d59f701a4df7ef74d036aa208f30a64980ece98b
wuhibe <wuhibetamire710@gmail.com>
2022-05-05 00:33:39 +0300

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" MRU caching """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache class inheriting from BaseCaching """
+    def __init__(self):
+        """ Initialize class """
+        super().__init__()
+        self.store = {}
+        self.recent = 0
+
+    def put(self, key, item):
+        """ Assign to the dictionary """
+        if key and item:
+            self.cache_data[key] = item
+            self.store[key] = self.recent
+            self.recent += 1
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            val = 0
+            # store key in temp to prevent removal
+            temp = key
+            # find the key with the highest recent value
+            for ky in self.store:
+                vl = self.store[ky]
+                if vl > val and ky != temp:
+                    val = vl
+                    key = ky
+            print("DISCARD: {}".format(key))
+            # remove the key from cache and store
+            self.cache_data.pop(key)
+            self.store.pop(key)
+
+    def get(self, key):
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        self.store[key] = self.recent
+        self.recent += 1
+        return self.cache_data.get(key)
